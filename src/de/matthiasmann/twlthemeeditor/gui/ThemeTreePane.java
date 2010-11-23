41a228851497ca40330db3529fa6a94ef9e26266
Matthias Mann <devnull@localhost>
2010-11-23 10:33:50 +0100

    added context menu to theme tree

--- src/de/matthiasmann/twlthemeeditor/gui/ThemeTreePane.java
+++ src/de/matthiasmann/twlthemeeditor/gui/ThemeTreePane.java
@@ -29,6 +29,7 @@
  */
 package de.matthiasmann.twlthemeeditor.gui;
 
+import de.matthiasmann.twl.Event;
 import de.matthiasmann.twl.SimpleDialog;
 import de.matthiasmann.twl.BoxLayout;
 import de.matthiasmann.twl.DialogLayout;
@@ -125,12 +126,19 @@ public class ThemeTreePane extends DialogLayout {
                     treeTable.setRowExpanded(row, !treeTable.isRowExpanded(row));
                 }
             }
+            public void mouseRightClick(int row, int column, Event evt) {
+                if(row >= 0 && row < treeTable.getNumRows()) {
+                    showTreeTablePopupMenu(row, evt);
+                }
+            }
             public void columnHeaderClicked(int column) {
             }
         });
         table.addCallback(new Callback() {
             public void mouseDoubleClicked(int row, int column) {
                 clearFilterAndJumpToRow(row);
+            }
+            public void mouseRightClick(int row, int column, Event evt) {
             }
             public void columnHeaderClicked(int column) {
             }
@@ -185,6 +193,39 @@ public class ThemeTreePane extends DialogLayout {
         }
     }
 
+    void showTreeTablePopupMenu(int row, Event evt) {
+        TreeTableNode nodeFromRow = treeTable.getNodeFromRow(row);
+        if(nodeFromRow instanceof ThemeTreeNode) {
+            ThemeTreeNode node = (ThemeTreeNode)nodeFromRow;
+            Menu menu = new Menu();
+
+            if(node.getParent() instanceof ThemeTreeNode) {
+                ThemeTreeNode parentNode = (ThemeTreeNode)node.getParent();
+                List<ThemeTreeOperation> parentOperations = parentNode.getOperations();
+                if(hasOperation(parentOperations, "opNewNode", null)) {
+                    // TODO: need a way to specify the insert location
+                }
+            }
+            
+            List<ThemeTreeOperation> operations = node.getOperations();
+            Menu createSubMenu = new Menu("New child");
+            if(createSubMenu(createSubMenu, node, operations, "opNewNode", null) > 0) {
+                menu.add(createSubMenu);
+            }
+            if(hasOperation(operations, null, "opDeleteNode") || hasOperation(operations, null, "opCloneNode")) {
+                if(menu.getNumElements() > 0) {
+                    menu.addSpacer();
+                }
+                createSubMenu(menu, node, operations, null, "opCloneNode");
+                createSubMenu(menu, node, operations, null, "opDeleteNode");
+            }
+
+            if(menu.getNumElements() > 0) {
+                menu.openPopupMenu(treeTable, evt.getMouseX(), evt.getMouseY());
+            }
+        }
+    }
+
     public TreeTableNode getSelected() {
         return selected;
     }
@@ -199,6 +240,30 @@ public class ThemeTreePane extends DialogLayout {
         }
         table.setModel(filteredModel);
         updateFilter();
+    }
+
+    private boolean hasOperation(List<ThemeTreeOperation> operations, String groupID, String actionID) {
+        for(final ThemeTreeOperation operation : operations) {
+            if(groupID != null && groupID.equals(operation.getGroupID())) {
+                return true;
+            }
+            if(actionID != null && actionID.equals(operation.getActionID())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int createSubMenu(Menu menu, final ThemeTreeNode node, List<ThemeTreeOperation> operations, String groupID, String actionID) {
+        int count = 0;
+        for(final ThemeTreeOperation operation : operations) {
+            if((groupID != null && groupID.equals(operation.getGroupID())) ||
+                    (actionID != null && actionID.equals(operation.getActionID()))) {
+                menu.add(createMenuAction(node, operation));
+                count++;
+            }
+        }
+        return count;
     }
 
     private Menu createButtons(final ThemeTreeNode node, List<ThemeTreeOperation> operations) {
@@ -219,18 +284,24 @@ public class ThemeTreePane extends DialogLayout {
                 }
             }
 
-            MenuAction action = new MenuAction();
-            action.setTheme(operation.getActionID());
-            action.setEnabled(operation.isEnabled());
-            action.setCallback(new Runnable() {
-                public void run() {
-                    queryOperationParameter(node, operation);
-                }
-            });
+            MenuAction action = createMenuAction(node, operation);
 
             ((subPopupMenu != null) ? subPopupMenu : menu).add(action);
         }
         return menu;
+    }
+
+    private MenuAction createMenuAction(final ThemeTreeNode node, final ThemeTreeOperation operation) {
+        MenuAction action = new MenuAction();
+        action.setTheme(operation.getActionID());
+        action.setEnabled(operation.isEnabled());
+        action.setCallback(new Runnable() {
+
+            public void run() {
+                queryOperationParameter(node, operation);
+            }
+        });
+        return action;
     }
 
     private static final MessageLog.Category CAT_THEME_TREE_OPERATION =
